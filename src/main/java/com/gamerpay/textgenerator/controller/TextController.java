2831afb4d3c33b11f48ce6d0004d03992ecd17f8
Tsaghik Khachatryan <khachatryan.tsaghik@umbrella-trade.com>
2023-08-28 12:15:43 +0400

    changed autowiring

--- src/main/java/com/gamerpay/textgenerator/controller/TextController.java
+++ src/main/java/com/gamerpay/textgenerator/controller/TextController.java
@@ -2,17 +2,20 @@ package com.gamerpay.textgenerator.controller;
 
 import com.gamerpay.textgenerator.data.Text;
 import com.gamerpay.textgenerator.service.TextEntryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/text")
 public class TextController {
 
-    @Autowired
-    private TextEntryService textEntryService;
+    private final TextEntryService textEntryService;
 
     @PostMapping("/save")
     public ResponseEntity<Text> saveText(@RequestBody Text text) {
